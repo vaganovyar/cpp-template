c6879c05dad3fbd32f2f65602da9080c7351bc3a
Yaroslav Vaganov <vaganovyar@gmail.com>
2020-09-17 17:25:18 +0500

    Decorated the code

--- template.cpp
+++ template.cpp
@@ -23,11 +23,11 @@ const ll INF_ = -9223372036854775806;
 double pi = 2 * acos(0.0);
 
 //length of two points
-double lenght(ii a, ii b){
+double lenght(ii a, ii b) {
     return sqrt(pow((a.first - b.first), 2) + pow((a.second - b.second), 2));
 }
 //upper ceil of a / b
-ll rmod(ll a, ll b){
+ll rmod(ll a, ll b) {
     double x = a / b;
     if (x > int(x))
         x = int(x) + 1;
@@ -35,28 +35,27 @@ ll rmod(ll a, ll b){
     return x;
 }
 //-1 if s not in vector, else index
-ll binSearch(vi n, ll s){
+ll binSearch(vi n, ll s) {
     sort(n.begin(), n.end());
     ull left = 0;
     ull right = n.size();
     ull mid = rmod((right + left), 2);
     ull last_mid = INF_U - 1;
-    while (n[mid] != s){
+    while (n[mid] != s) {
         if (n[mid] < s)
             left = mid;
         else
             right = mid;
         last_mid = mid;
         mid = rmod((left + right), 2);
-        if (last_mid == mid){
+        if (last_mid == mid)
             return -1;
-        }
     }
     return mid;
 }
 //nok(a, b) = (a * b) / nod(a, b)
-ull nod(ull a, ull b){
-    while (a % b != 0){
+ull nod(ull a, ull b) {
+    while (a % b != 0) {
         ull c = a % b;
         a = b;
         b = c;
@@ -64,59 +63,53 @@ ull nod(ull a, ull b){
     return b;
 }
 //sum of vector
-ll v_sum(vi a){
+ll v_sum(vi a) {
     ll s = 0;
-    for (ll i = 0; i < a.size(); i++){
+    for (ll i = 0; i < a.size(); i++)
         s += a[i];
-    }
     return s;
 }
 //minimum number in vector
-ll v_min (vi a){
+ll v_min (vi a) {
     ll minimum;
     minimum = INF;
-    for (ll i = 0; i < a.size(); i++){
+    for (ll i = 0; i < a.size(); i++)
         if (a[i] < minimum)
             minimum = a[i];
-    }
     return minimum;
 }
 //maximum number in vector
-ll v_max (vi a){
+ll v_max (vi a) {
     ll maximum;
     maximum = INF_;
-    for (ll i = 0; i < a.size(); i++){
+    for (ll i = 0; i < a.size(); i++)
         if (a[i] < maximum)
             maximum = a[i];
-    }
     return maximum;
 }
 
 //debug
 #define watch(x) cout << (#x) << " is " << (x) << endl;
-void watch_v(vi a){
-    for (ull i = 0; i < a.size(); i++){
+void watch_v(vi a) {
+    for (ull i = 0; i < a.size(); i++)
         cout << a[i] << " ";
-    }
     cout << endl;
 }
-void watch_v_p(vii a){
-    for (ull i = 0; i < a.size(); i++){
+void watch_v_p(vii a) {
+    for (ull i = 0; i < a.size(); i++)
         cout << "{" << a[i].first << ", " << a[i].second << "} ";
-    }
     cout << endl;
 }
-void watch_matrix(matrix a){
-    for (ull i = 0; i < a.size(); i++){
-        for (ull j = 0; j < a[i].size(); j++){
+void watch_matrix(matrix a) {
+    for (ull i = 0; i < a.size(); i++) {
+        for (ull j = 0; j < a[i].size(); j++)
             cout << a[i][j] << " ";
-        }
         cout << endl;
     }
     cout << endl;
 }
 
-void code(){
+void code() {
     //your code here
 
 }
@@ -126,8 +119,7 @@ int main() {
     cin.tie(0);
     ull num = 1;
     //cin >> num;
-    for (int i = 0; i < num; i++){
+    for (int i = 0; i < num; i++)
         code();
-    }
     return 0;
 }
